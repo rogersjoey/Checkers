1d1eda52ff617840ec39442f38b162e63d05146d
Joseph Rogers <RogersJoseph@JohnDeere.com>
2021-01-08 15:05:52 -0600

    select checkers better

--- board.js
+++ board.js
@@ -1,41 +1,114 @@
-function cellTemp(rowNum, colNum) {
-    rowNum %2 == 1? rowEven = false: rowEven = true;
-    colNum %2 == 1? colEven = false: colEven = true;
-    if (colEven != rowEven) {
-        let cellString = `<div id='cell-${rowNum}-${colNum}' class='cell black' onclick=toggle(this)>`
-        if (rowNum<=3){
-            cellString = cellString +
-            `<div class="checker red-checker"></div>
-            <div class="checker black-checker" hidden></div>`}
-        else if (rowNum>=6){
-            cellString = cellString +
-            `<div class="checker red-checker" hidden></div>
-            <div class="checker black-checker"></div>`
-        } else {cellString = cellString +
-            `<div class="checker red-checker" hidden></div>
-            <div class="checker black-checker" hidden></div>`
-        }
-        return cellString + `</div>`
+$(document).ready(function() {
+    console.log('document ready')
+    $('board-container').html(renderBoard())
+    renderCheckers()
+    $('.checker').click(selectChecker)
+})
+
+function toggle() {
+    let checker = $(this).children().first()
+    checker.toggle()
+    if(!checker.is(":checker")){
+        switchColor(checker);
+    }
+}
+
+function switchColor (checker) {
+    if (checker.hasClass('black-checker')){
+        checker.removeClass('black-checker')
+        checker.addClass('white-checker')
     } else {
-        return `<div id='cell-${rowNum}-${colNum}' class='cell red'></div>`
+        checker.addClass('black-checker')
+        checker.removeClass('white-checker')
+    }
+}   
+
+function renderBoard(){
+    return `
+        ${renderRow(1)}
+        ${renderRow(2)}
+        ${renderRow(3)}
+        ${renderRow(4)}
+        ${renderRow(5)}
+        ${renderRow(6)}
+        ${renderRow(7)}
+        ${renderRow(8)}
+    `
+}
+
+function renderRow(rowNum){
+    return `
+        <div id="row-${rowNum}" class="row">
+            ${renderCell(rowNum,1)}
+            ${renderCell(rowNum,2)}
+            ${renderCell(rowNum,3)}
+            ${renderCell(rowNum,4)}
+            ${renderCell(rowNum,5)}
+            ${renderCell(rowNum,6)}
+            ${renderCell(rowNum,7)}
+            ${renderCell(rowNum,8)}
+        </div>
+    `
+}
+
+function renderCell(rowNum, cellNum) {
+    if(cellColor(cellNum,rowNum) === 'black') {
+        return `<div id="cell-${rowNum}-${cellNum}" class="cell black"></div>`
+    } else {
+        return `<div id="cell-${rowNum}-${cellNum}" class="cell red"></div>`
     }
 }
 
-function rowTemp(rowNum){
-    let rowString = ''
-    rowString = rowString + `<div id="row-${rowNum}" class="row">`
-    for (let cellNum=1;cellNum<=8;cellNum++){
-        rowString = rowString + cellTemp(rowNum,cellNum)
-    }
-    rowString = rowString+ `</div>`
-    return rowString
-} 
-
-function boardTemp(){
-    let boardString =''
-    for (let rowNum=1;rowNum<=8;rowNum++){
-        boardString = boardString + rowTemp(rowNum)
-        console.log(boardString)
-    }
-    return boardString
+function parity(num) {
+    return (num%2 == 0) ? 'even' : 'odd'
 }
+
+function cellColor(cellNum,rowNum){
+    return parity(cellNum) == parity(rowNum)? 'red' : 'black'
+}
+
+
+
+
+
+// function cellTemp(rowNum, colNum) {
+//     rowNum %2 == 1? rowEven = false: rowEven = true;
+//     colNum %2 == 1? colEven = false: colEven = true;
+//     if (colEven != rowEven) {
+//         let cellString = `<div id='cell-${rowNum}-${colNum}' class='cell black' onclick=toggle(this)>`
+//         if (rowNum<=3){
+//             cellString = cellString +
+//             `<div class="checker red-checker"></div>
+//             <div class="checker black-checker" hidden></div>`}
+//         else if (rowNum>=6){
+//             cellString = cellString +
+//             `<div class="checker red-checker" hidden></div>
+//             <div class="checker black-checker"></div>`
+//         } else {cellString = cellString +
+//             `<div class="checker red-checker" hidden></div>
+//             <div class="checker black-checker" hidden></div>`
+//         }
+//         return cellString + `</div>`
+//     } else {
+//         return `<div id='cell-${rowNum}-${colNum}' class='cell red'></div>`
+//     }
+// }
+
+// function rowTemp(rowNum){
+//     let rowString = ''
+//     rowString = rowString + `<div id="row-${rowNum}" class="row">`
+//     for (let cellNum=1;cellNum<=8;cellNum++){
+//         rowString = rowString + cellTemp(rowNum,cellNum)
+//     }
+//     rowString = rowString+ `</div>`
+//     return rowString
+// } 
+
+// function boardTemp(){
+//     let boardString =''
+//     for (let rowNum=1;rowNum<=8;rowNum++){
+//         boardString = boardString + rowTemp(rowNum)
+//         // console.log(boardString)
+//     }
+//     return boardString
+// }
--- checkers.js
+++ checkers.js
@@ -1,18 +1,46 @@
-function toggle(cell) {
-    let red = cell.children[0]
-    let black = cell.children[1]
+let selectedChecker = undefined
 
-    if(!red.hidden && black.hidden){
-        // red on, turn off
-        red.hidden = true
-        black.hidden = false
-    } else if(red.hidden && !black.hidden){
-        //black on, turn off
-        red.hidden = true
-        black.hidden = true
-    } else if(red.hidden && black.hidden){
-        //both are off turn black on 
-        red.hidden = false
-        black.hidden = true
-    } 
+let checkers = [
+    {row:1, cell:2, color: 'red'},
+    {row:1, cell:4, color: 'red'},
+    {row:1, cell:6, color: 'red'},
+    {row:1, cell:8, color: 'red'},
+    {row:2, cell:1, color: 'red'},
+    {row:2, cell:3, color: 'red'},
+    {row:2, cell:5, color: 'red'},
+    {row:2, cell:7, color: 'red'},
+    {row:3, cell:2, color: 'red'},
+    {row:3, cell:4, color: 'red'},
+    {row:3, cell:6, color: 'red'},
+    {row:3, cell:8, color: 'red'},
+    {row:6, cell:1, color: 'black'},
+    {row:6, cell:3, color: 'black'},
+    {row:6, cell:5, color: 'black'},
+    {row:6, cell:7, color: 'black'},
+    {row:7, cell:2, color: 'black'},
+    {row:7, cell:4, color: 'black'},
+    {row:7, cell:6, color: 'black'},
+    {row:7, cell:8, color: 'black'},
+    {row:8, cell:1, color: 'black'},
+    {row:8, cell:3, color: 'black'},
+    {row:8, cell:5, color: 'black'},
+    {row:8, cell:7, color: 'black'},
+]
+
+function renderCheckers(){
+    console.log('rendering checkers')
+    for(let i=0; i<checkers.length; i++){
+        let checker = checkers[i];
+        console.log(checker)
+        $(`#cell-${checker.row}-${checker.cell}`).html(renderChecker(checker.color))
+    }
+}
+
+function renderChecker(color) {
+    return `<div class="checker ${color}-checker"></div`
+}
+
+function selectChecker(checker){
+    console.log("selecting checker: ", checker)
+    selectedChecker = checker
 }
